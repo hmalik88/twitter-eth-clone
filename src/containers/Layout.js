b0a98990c616ec7bbd7576688cd24540e1cff291
hmalik88 <hbmalik88@gmail.com>
2020-11-08 15:53:59 -0500

    Added layout page to hold sidebar, feed and right sidepanel components. Layout takes a page prop to indicate if the feed component should render a tweetbox or a profile info component.

---
+++ src/containers/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid'
+import SideBar from '../components/SideBar';
+import Feed from '../components/Feed';
+import RightPanel from '../components/RightPanel';
+
+function Layout(props) {
+    return(
+        <>
+            <Grid container>
+                <Grid item sm={3}><SideBar /></Grid>
+                <Grid item sm={3}><Feed page={props.page} /></Grid>
+                <Grid item sm={3}><RightPanel /></Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default Layout;
