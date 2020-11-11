9746ddd4d1c49b6efe5e134a208c8e096e92339a
hmalik88 <hbmalik88@gmail.com>
2020-11-11 06:48:55 -0500

    added in code to fill out the login page form.

---
+++ src/components/LoginPageForm.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+
+function LoginPageForm() {
+    return(
+        <form>
+            <TextField 
+                variant="filled"
+                label="Email"
+                InputLabelProps={{
+                    style: { 
+                        color: 'rgb(29,161,242)', 
+                        fontSize: '15px'
+                    }
+                }}
+                InputProps={{
+                    style: {
+                        backgroundColor: 'rgb(25,39,52)', 
+                        color: 'white', 
+                        font: '700 13.3333px Arial'
+                    }
+                }}
+            />
+            <TextField 
+                variant="filled"
+                label="Password"
+                type="password"
+                InputLabelProps={{
+                    style: { 
+                        color: 'rgb(29,161,242)', 
+                        fontSize: '15px'
+                    }
+                }}
+                InputProps={{
+                    style: {
+                        backgroundColor: 'rgb(25,39,52)', 
+                        color: 'white', 
+                        font: '700 13.3333px Arial'
+                    }
+                }}
+            />
+            <button type="submit" className="login-form-btn">
+                <span>Log in</span>
+            </button>
+        </form>
+    )
+}
+
+export default LoginPageForm;
