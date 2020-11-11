94b29e23c275e919ac091d1935d7fcf60842574d
hmalik88 <hbmalik88@gmail.com>
2020-11-11 06:46:53 -0500

    fixed spacing.

--- src/components/LandingLoginForm.js
+++ src/components/LandingLoginForm.js
@@ -4,21 +4,43 @@ import TextField from '@material-ui/core/TextField';
 function LandingLoginForm() {
     return (
         <div>
-            <form>
+            <form className="landing-form">
                 <TextField 
                     variant="filled"
                     label="Email"
                     style={{paddingRight: '15px'}}
-                    InputLabelProps={{style: { color: 'rgb(29,161,242)', fontSize: '15px'}}}
-                    InputProps={{style: {backgroundColor: 'rgb(25,39,52)', color: 'white', font: '700 13.3333px Arial'}}}
+                    InputLabelProps={{
+                        style: { 
+                            color: 'rgb(29,161,242)', 
+                            fontSize: '15px'
+                        }
+                    }}
+                    InputProps={{
+                        style: {
+                            backgroundColor: 'rgb(25,39,52)', 
+                            color: 'white', 
+                            font: '700 13.3333px Arial'
+                        }
+                    }}
                 />
                 <TextField 
                     variant="filled"
                     label="Password"
                     type="password"
                     style={{paddingRight: '15px'}}
-                    InputLabelProps={{style: { color: 'rgb(29,161,242)', fontSize: '15px'}}}
-                    InputProps={{style: {backgroundColor: 'rgb(25,39,52)', color: 'white', font: '700 13.3333px Arial'}}}
+                    InputLabelProps={{
+                        style: { 
+                            color: 'rgb(29,161,242)', 
+                            fontSize: '15px'
+                        }
+                    }}
+                    InputProps={{
+                        style: {
+                            backgroundColor: 'rgb(25,39,52)', 
+                            color: 'white', 
+                            font: '700 13.3333px Arial'
+                        }
+                    }}
                 />
                 <button type="submit" className="landing-button">
                     <span>Log in</span>
