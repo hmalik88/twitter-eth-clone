fbb3b468cf25d1f3a3bb2a6078f558ebe40400b9
hmalik88 <hbmalik88@gmail.com>
2020-11-11 06:48:11 -0500

    added in html for login page.

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+    icon: {
+        width: "39px",
+        color: "white"
+    }
+});
 
 function LoginPage() {
+    const classes = useStyles();
     return (
         <>
-            Login here.
+            <div className="login-bg">
+                <div className='login-form-container'>
+                    <TwitterIcon className={classes.icon} />
+                    <h1 className="login-text"><span>Log in to notTwitter</span></h1>
+                </div>
+            </div>
         </>
     )
 }
