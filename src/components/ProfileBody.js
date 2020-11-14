5289450c147f6f0df51545fae3547ba31adffb76
hmalik88 <hbmalik88@gmail.com>
2020-11-14 10:42:22 -0500

    added jsx to the profile body component

--- src/components/ProfileBody.js
+++ src/components/ProfileBody.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import { Avatar } from "@material-ui/core";
 
 function ProfileBody() {
     return(
+        <div className="profile-body-container">
+            <div className="profile-banner">
 
+            </div>
+            <div className="profile-info-container">
+                <div className="edit-btn-container">
+                    <button>Edit Profile</button>
+                </div>
+                <div className="info-container">
+                    <span>Hassan</span>
+                    <span>@shizbksillest</span>
+                    <div className="user-info">
+                        <img src={} alt="" className="pin" />
+                        <span>
+                            Brooklyn, NY
+                        </span>
+                        <img src={} alt="" className="balloon" />
+                        <span>
+                            Born December 19, 1988
+                        </span>
+                        <img src={} alt="" className="calendar" />
+                        <span>
+                            Joined May 2012
+                        </span>
+                    </div>
+                    <div className="follower-info">
+                        <div>
+                            <span>23</span> Following
+                        </div>
+                        <div>
+                            <span>14</span> Following
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Avatar />
+        </div>
     )
 }
 
