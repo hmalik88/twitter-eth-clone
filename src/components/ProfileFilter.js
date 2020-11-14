44d0623aaafb21f432110bd7721cae9bd19f94fd
hmalik88 <hbmalik88@gmail.com>
2020-11-14 15:18:35 -0500

    removed unneccessary asset and stubbed out profile filter component and added css for filter component.

---
+++ src/components/ProfileFilter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function ProfileFilter() {
+    return(
+        <div className="profile-filter">
+            <div className="tweets-filter">
+                <span>Tweets</span>
+            </div>
+            <div className="tweeths-filter">
+                <span>Tweeths</span>
+            </div>
+            <div className="likes-filter">
+                <span>Likes</span>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileFilter;
+
